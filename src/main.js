18277b120dd50be94bfde1194b3b624003235a23
Thayana Mamoré <thayanacmamore@gmail.com>
2019-09-29 10:11:23 -0300

    configurando para utilizar axios e Vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import vuetify from './plugins/vuetify';
+
+axios.defaults.baseURL = "https://pokeapi.com/api/v2"
+Vue.prototype.axios = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
