dc9d17b7d6284ed81074def4748344b1eec55f5e
Thayana Mamoré <thayanacmamore@gmail.com>
2019-09-30 18:04:11 -0300

    Arrumando rodapé

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,37 @@
 <template>
-    <v-footer app>
-        <v-flex class="nes-container is-rounded">
-            &copy; {{ new Date().getFullYear() }} —
-                <strong>
-                    Thayana Mamoré
-                </strong>
-        </v-flex>
-    </v-footer>
+  <v-footer row app class="pa-1">
+    <v-layout wrap class="nes-container is-rounded pa-0 ma-0">
+      <v-flex>
+        <small>
+          &copy; {{ new Date().getFullYear() }} —
+          <strong>Thayana Mamoré</strong>
+        </small>
+      </v-flex>
+      <v-flex class="text-right">
+        <a href="https://github.com/thauska" target="_blank" class="pr-2">
+          <i class="nes-icon github is-small"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/thayanacmamore/" target="_blank">
+          <i class="nes-icon linkedin is-small"></i>
+        </a>
+      </v-flex>
+    </v-layout>
+  </v-footer>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
-<style>
+<style scoped>
+
+div .flex {
+  margin: 0 10px 0 10px;
+}
+
+@media (max-width: 375px) {
+  .v-footer {
+    font-size: 0.7rem;
+  }
+}
 </style>
